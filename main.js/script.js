3d2132117c853ce1a0e2241244bcdcdbab7d46e6
Dmitry <golubov123666@gmail.com>
2025-02-03 16:04:10 +0300

    Realization for and if in programm

--- main.js/script.js
+++ main.js/script.js
@@ -8,12 +8,24 @@ const personalMovieDB = {
     private: false
 };
 
-const a = prompt("Один из просмотренных последних фильмов",""),
-      b = prompt("На сколько оцените его",""),
-      c = prompt("Один из просмотренных последних фильмов",""), 
-      d = prompt("На сколько оцените его","");
+for(let i = 0; i < 2; i++){
+    const a = prompt("Один из просмотренных последних фильмов",""),
+          b = prompt("На сколько оцените его","");
+     if(a != '' && b != '' && a != null && b != null && a.length < 50){
+     personalMovieDB.movies[a] = b;
+     }else{
+        i--;
+     }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if(numberOfFilms < 10){
+    console.log("Просмотренно мало фильмов")
+}else if(numberOfFilms > 10 && numberOfFilms <30){
+    console.log("Вы классический зритель")
+}else if(numberOfFilms > 30 ){
+    console.log("Вы киноман")
+}else{
+    console.log("Произошла ошибка")
+}
 
 console.log(personalMovieDB)
